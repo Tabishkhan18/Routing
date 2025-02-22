afc0399ed269e6715dd24c323713d012e9cf6cf3
Tabishkhan18 <tabishkhan1811@gmail.com>
2025-02-22 15:50:53 +0530

    update hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,36 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import User from './components/User'
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import User from './components/User';
 import './App.css';
 
-
-
 function App() {
-  const router = createBrowserRouter([
-
-  {
-    path: "/",
-    element: <> <Navbar /> <Home/> </> 
-  },
-  {
-    path: "/about",
-    element:  <> <Navbar />  <About/> </>
-  },
-  {
-    path: "/contact",
-    element: <> <Navbar />  <Contact/>  </>
-  },
-  {
-
-    path: "/user/:username",
-    element: <> <Navbar />  <User/>  </>
-  },
-
-
-
-  ])
-
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <> <Navbar /> <Home/> </> 
+    },
+    {
+      path: "about",
+      element:  <> <Navbar />  <About/> </>
+    },
+    {
+      path: "contact",
+      element: <> <Navbar />  <Contact/>  </>
+    },
+    {
+      path: "user/:username",
+      element: <> <Navbar />  <User/>  </>
+    }
+  ]);
 
   return (
     <>
       <RouterProvider router={router}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
